6eac12284f6e522cf251b8c9a654349cec516e76
vizuetcruzfi <vizuetcruz_fi@outlook.com>
2024-06-24 17:28:23 -0600

    Components description in 'App.js'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+// Gallery Components
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+// Component Profile
+
+// Función del componente Profile()
+function Profile() {
+return (
+  <img
+      src="https://i.imgur.com/MK3eW3As.jpg"
+      alt="Katherine Johnson"
+    />
   );
 }
 
-export default App;
+/* 
+
+Este componente será reutilizado dentro del marcado de la función Profile()
+
+*/
+
+
+// Component Gallery
+
+// export defaul - sirve para exportar un componente
+// function Gallery() - Es la función que creas para el componente "Gallery"
+export default function Gallery() {
+  return(
+    <section>
+      <h1>Cientificos increibles</h1>
+      <Profile/> 
+      <Profile/>
+      <Profile/>
+    </section>
+  );
+}
+
+/* 
+
+<section>      
+  <h1>Cientificos increibles</h1>
+  <Profile/> 
+  <Profile/>
+  <Profile/>
+</section> 
+  
+Lo anterior es una etiqueta que se parece a HTML, pero en realidad es js, con una sintaxis especial, JSX, que permite ingresar marcado dentro de js. 
+
+El marcado debe estar en la misma linea que el "return", de no ser asi, deberás colocarlo dentro de parentesis.
+
+*/
+
+// Lo que ve el navegador
+
+/* 
+
+<section>
+  <h1>Científicos increíbles</h1>
+  <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />
+  <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />
+  <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />
+</section>
+
+La diferencia principal del tipado en la sintaxis JSX, es que las etiquetas se escriben en minusculas, ademas de que las etiquetas Profile se reemplazaron por las etiquetas img, ya que el componente Profile nos retorna una etiqueta img.
+*/  
+
+// Componentes anidados
+
+/*
+
+export default function Gallery() {
+  // 🔴 ¡Nunca definas un componente dentro de otro componente!
+  function Profile() {
+    // ...
+  }
+  // ...
+}
+
+Los componentes anidados no funcionan, la forma correcta sería la siguiente.
+
+export default function Gallery() {
+  // ...
+}
+
+// ✅ Declara los componentes en el primer nivel
+function Profile() {
+  // ...
+}
+
+*/
